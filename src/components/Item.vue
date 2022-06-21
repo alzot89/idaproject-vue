b17d911325e9f0ae940cdc87be7bb061a991840a
Sanches <sanches-zotov@yandex.ru>
2022-06-21 12:21:14 +0300

    separate component Item

---
+++ src/components/Item.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <img class="item__image" :src="item.imageUrl" :alt="item.name">
+    <h2 class="item__name">{{ item.name }}</h2>
+    <p class="item__description">{{ item.description }}</p>
+    <p>{{ item.price }}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: ['item']
+}
+
+</script>
+
+<style>
+.item__image {
+  width: 100%;
+  object-fit: contain;
+}
+
+.item__name {
+  font-family: 'Source Sans Pro';
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.25;
+}
+
+.item__description {
+  font-family: 'Source Sans Pro';
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 1.25;
+}
+</style>
--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -1,20 +1,19 @@
 <template>
 
   <ul>
-    <li v-for="(item, index) in items" :key="index">
-      <img class="item__image" :src="item.imageUrl" alt="">
-      <h2 class="item__name">{{ item.name }}</h2>
-      <p class="item__description">{{ item.description }}</p>
-      <p>{{ item.price }}</p>
-    </li>
+    <Item v-for="(item, index) in items" :key="index" :item="item" />
   </ul>
 
 </template>
 
 <script>
+import Item from '../components/Item.vue';
 export default {
   name: 'ItemsList',
-  props: ['items']
+  props: ['items'],
+  components: {
+    Item
+  }
 }
 
 </script>
@@ -27,24 +26,5 @@ ul {
   display: grid;
   grid-template-columns: repeat(3, 332px);
   gap: 16px;
-}
-
-.item__image {
-  width: 100%;
-  object-fit: contain;
-}
-
-.item__name {
-  font-family: 'Source Sans Pro';
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 1.25;
-}
-
-.item__description {
-  font-family: 'Source Sans Pro';
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 1.25;
 }
 </style>
